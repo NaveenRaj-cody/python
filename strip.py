61d2aab98b04397a0e616b12a03cc7d657446407
NaveenRaj-cody <naveenrajofficial1@gmail.com>
2024-12-28 10:55:00 +0530

    Create strip.py

---
+++ strip.py
@@ -0,0 +1,15 @@
+"""a=int(input())
+b=int(input())
+if(a*b<=1000):
+    print("The result is ",a*b)
+else:
+    print(a+b)
+    
+for i in range(11):
+    print(f"Current Number {i} Previous Number {i-1} sum: {i+(i-1)}" if i!=0 else f"Current Number {i} Previous Number {i} sum: {i}" )"""
+
+s="        Don't sleep     "
+print(s)
+print(s.lstrip("eep     "))
+print(s.rstrip("eep     "))
+print(s.strip("eep     "))
